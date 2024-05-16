dbc6a71ccef4486e0e49d0b79b320f65ee060154
Tobias Olsson <totte174@gmail.com>
2024-05-16 13:24:26 +0200

    fixed python script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # minesweeper-probability
+
+## Future features
+generalize to N-dimensional minesweeper
+generalize to allow enabling of wrapping edges
+
+
+## Possible improvements to winrate
+brute force??
+
+## Possible improvements to performance
+Improvements to equation reduction to possibly split the really hard border which cause the solver to run out of memory
--- test_parameters.py
+++ test_parameters.py
@@ -1,9 +1,10 @@
 import numpy as np, subprocess
 if __name__ == "__main__":
-    param_values = np.arange(0, 1.0, 0.05)
-    NUM_GAMES = 100_000
+    param_values = np.arange(0.8, 0.85, 0.05)
+    NUM_GAMES = 1_000_000
     param = "eta"
     for param_v in param_values:
-        wins = int(subprocess.run(['./bin/mssolve', f'--test={NUM_GAMES}', f'--{param}="{param_v}"'], capture_output=True).stdout.decode())
+        print(['./bin/mssolve', f'--{param}={param_v:.2f}', f'--test={NUM_GAMES}'])
+        wins = int(subprocess.run(['./bin/mssolve', f'--{param}={param_v:.2f}', f'--test={NUM_GAMES}'], capture_output=True).stdout.decode())
         p_hat = wins / NUM_GAMES
         print(param_v, p_hat)
